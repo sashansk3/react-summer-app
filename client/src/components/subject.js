7e579d3beb4fd4604b19aea96497544a478ae910
sashansk3 <sashansk3@gmail.com>
2020-07-25 12:07:17 +0700

    change structure of JSX code

--- client/src/components/subject.js
+++ client/src/components/subject.js
@@ -10,24 +10,18 @@ export default function Subject(props){
     dispatch = useDispatch(),
     openPopUpAction = () => dispatch(openPopUp(props.subject))
 
-  let {labs, name, teachers, type, week} = props.subject
+  let {labs, name, week} = props.subject
   return (
     <div className="subject" onClick={openPopUpAction}>
       <div className="subject_name">
         {name}
       </div>
-      <div className="subject_week">
-        {week}
-      </div>
-      <div className="subject_teachers">
-        {teachers[0]}
-        {teachers[1]}
-      </div>
       <div className="subject_labs">
         0/{labs}
       </div>
-      <div className="subject_type">
-        {type}
+      <div className={`subject_week`}>
+        <h6>week:</h6>
+        <text className={`subject_week__${week}`}>{week}</text>
       </div>
     </div>
   )
--- client/src/components/subjects.js
+++ client/src/components/subjects.js
@@ -1,25 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 import { useSelector, useDispatch, shallowEqual } from "react-redux"
 
 import Subject from './subject'
-import PopUp from './popup';
-import { getSubjects, closePopUp, deleteSubject, setEditFlag } from "../actions/action.subject"
+import { getSubjects, closePopUp, setEditFlag } from "../actions/action.subject"
 import "../styles/subjects.scss"
 
 export default function Subjects() {
   const dispatch = useDispatch()
   useEffect(() => dispatch(getSubjects()), [])
-  
-  const 
-    isOpenFlag = useSelector(store => store.subjects.isOpen, shallowEqual),
-    subjects   = useSelector(store => store.subjects.subjects, shallowEqual),
-    editFlag   = useSelector(store => store.subjects.editFlag, shallowEqual),
-    subject    = useSelector(store => store.subjects.subject, shallowEqual);
 
   const 
-    redirectToAddSubject  = () => dispatch(setEditFlag("add")),
-    redirectToEditSubject = () => dispatch(setEditFlag("edit"));
+    subjects = useSelector(store => store.subjects.subjects, shallowEqual),
+    editFlag = useSelector(store => store.subjects.editFlag, shallowEqual);
+
+  const redirectToAddSubject = () => dispatch(setEditFlag("add"))
 
   if(editFlag === "add")
     return <Redirect to="/subjects/add"/>
@@ -27,48 +22,24 @@ export default function Subjects() {
     dispatch(closePopUp())
     return <Redirect to="/subjects/edit"/>
   }
-
-  let mapSubjects = subjects.map(subject => <Subject key = {subject.id} subject = {subject}/>)
   return (
     <div className="subjects">
       <div className="subjects-filters">
         <button onClick={redirectToAddSubject}> add Subject </button>
       </div>
+
       <div className="subjects-list">
-        {mapSubjects}
+        {subjects.map(subject => 
+          <Subject 
+            key     = {subject.id}
+            subject = {subject}
+          />
+        )}
       </div>
-      <PopUp
-        isOpen = {isOpenFlag}
-        close  = {() => dispatch(closePopUp())}
-      >
-        <div className="popup-subject">
-          <div className="popup-subject-header">
-            <div className="popup-subject-week">
-              week: {subject.week}
-            </div>
-          </div>
-          <div className="popup-subject-content">
-            <div className="popup-subject-teachers">
-              {subject.teachers?subject.teachers.map(teacher => <p>{teacher}</p>):""}
-            </div>
-          </div>
-          <div className="popup-subject-name">
-            {subject.name}
-          </div>
-          <div className="popup-subject-footer">
-            <div className="popup-subject-labs">
-              0/{subject.labs}
-            </div>
-            <div className="popup-subject-type">
-              Type: {subject.type}
-            </div>
-            <div className="popup-subject-buttons">
-              <button className="deleteBtn" onClick={() => dispatch(deleteSubject())}>delete</button>
-              <button className="eeditBtn" onClick={redirectToEditSubject}>edit</button>
-            </div>
-          </div>
-        </div>
-      </PopUp>
+
+      <div className="subjects_content">
+
+      </div>
     </div>
   )
 }
