5128f4623d209621576f9e38964606c433848e64
sashansk3 <sashansk3@gmail.com>
2020-08-20 19:25:15 +0700

    reduce code

--- client/src/components/lab.js
+++ client/src/components/lab.js
@@ -1,23 +1,15 @@
 import React from 'react'
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 
-import {openPopUp} from '../actions/labs.actions'
-import"../styles/lab.scss"
-export default function Lab(props) {
+import { openLabPopUp } from '../actions/labs.actions'
+import "../styles/lab.scss"
+export default function Lab({lab}) {
   const dispatch = useDispatch()
 
-  const openPopUpAction = () => dispatch(openPopUp(props.lab))
-
-  let style = "default"
-  if(props.lab.status === "inProgress"){
-    style = "yellow"
-  }
-  else if(props.lab.status === "completed"){
-    style = "green"
-  }
+  const openLabPopUpAction = () => dispatch(openLabPopUp(lab))
 
   return (
-    <div className={"lab back"+style} onClick={openPopUpAction}>
+    <div className={`lab lab__${lab.status}`} onClick={openLabPopUpAction}>
     </div>
   )
 }
